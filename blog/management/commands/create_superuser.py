e54b1ca3411744fe40b15eea0826461fcdd600a1
elbek <robertwerlko@gmail.com>
2022-08-30 00:13:49 +0500

    added management command and script entrypoint

---
+++ blog/management/commands/create_superuser.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    """ Для создания супер юзера """
+
+    def add_arguments(self, parser):
+        parser.add_argument('user_name', type=str)
+        parser.add_argument('password', type=str)
+
+    def handle(self, *args, **kwargs):
+        User.objects.create_superuser(
+            username=kwargs.get('user_name'),
+            password=kwargs.get('password')
+        )
+        self.stdout.write(self.style.SUCCESS('Successfully create superuser "%s"'))
